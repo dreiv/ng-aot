3ba313058b4c012c079fc591605107cc52e592f8
Andrei Voicu <Andrei-Florian.Voicu@haufe-lexware.com>
2016-10-25 15:28:18 +0300

    added environment variable support.

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,44 @@
 const webpack = require('webpack');
 
-module.exports = {
-    entry: {
+module.exports = (envOptions) => {
+    envOptions = envOptions || {};
+    const config = {
+        entry: {
         main: './src/main.ts'
     },
-    output: {
-        path: './dist',
-        filename: '[name].bundle.js',
-    },
-    resolve: {
-        extensions: ['.ts', '.js', '.html'],
-    },
-    module: {
-        rules: [
-            {test: /\.html$/, loader: 'raw'},
-            {test: /\.css$/, loader: 'raw'},
-            {test: /\.ts/, loaders: ['awesome-typescript-loader', 'angular2-template-loader']}
-        ]
-    },
-    devtool: '#source-map',
+        output: {
+            path: './dist',
+            filename: '[name].bundle.js',
+        },
+        resolve: {
+            extensions: ['.ts', '.js', '.html'],
+        },
+        module: {
+            rules: [
+                {test: /\.html$/, loader: 'raw'},
+                {test: /\.css$/, loader: 'raw'},
+                {test: /\.ts/, loaders: ['awesome-typescript-loader', 'angular2-template-loader']}
+            ]
+        },
+        devtool: '#source-map'
+    };
+
+    if (envOptions.MODE === 'prod') {
+        config.plugins = [
+            new webpack.optimize.UglifyJsPlugin({
+                beautify: false,
+                mangle: {
+                    screw_ie8: true,
+                    keep_fnames: true
+                },
+                compress: {
+                    warnings: false,
+                    screw_ie8: true
+                },
+                comments: false
+            }),
+        ];
+    }
+
+    return config;
 };
